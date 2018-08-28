b1ef2ccdecf4310d8d1b7d9b098e680b4009ed35
Harold Gay <harold.gay@acte-industrie.fr>
2018-08-28 10:39:08 +0200

    Update php example

--- test/test.php
+++ test/test.php
@@ -1,92 +1,49 @@
 <?php
 
-use Respect\Validation\Validator as v;
+// base class with member properties and methods
+class Vegetable {
 
-session_start();
+   var $edible;
+   var $color;
 
-require __DIR__ . '/../vendor/autoload.php';
+   function __construct($edible, $color="green")
+   {
+       $this->edible = $edible;
+       $this->color = $color;
+   }
 
-$app = new \Slim\App([
-  'settings' => [
-    'displayErrorDetails' => true,
-    'db' => [
-      'driver' => 'mysql',
-      'host' => 'localhost',
-      'database' => 'codecourse',
-      'username' => 'root',
-      'password' => '',
-      'charset' => 'utf8',
-      'collation' => 'utf8_unicode_ci',
-      'prefix' => '',
-    ]
-  ],
-]);
+   function is_edible()
+   {
+       return $this->edible;
+   }
 
-$container = $app->getContainer();
+   function what_color()
+   {
+       return $this->color;
+   }
 
-$capsule = new \Illuminate\Database\Capsule\Manager;
-$capsule->addConnection($container['settings']['db']);
-$capsule->setAsGlobal();
-$capsule->bootEloquent();
+} // end of class Vegetable
 
-$container['db'] = function ($container) use ($capsule) {
-    return $capsule;
-};
+// extends the base class
+class Spinach extends Vegetable {
 
-$container['auth'] = function ($container) {
-    return new \App\Auth\Auth;
-};
+   var $cooked = false;
 
-$container['flash'] = function ($container) {
-    return new \Slim\Flash\Messages();
-};
+   function __construct()
+   {
+       parent::__construct(true, "green");
+   }
 
-$container['view'] = function ($container) {
-    $view = new \Slim\Views\Twig(__DIR__ . '/../ressources/views', [
-    'cache' => false,
-  ]);
+   function cook_it()
+   {
+       $this->cooked = true;
+   }
 
-    $view->addExtension(new \Slim\Views\TwigExtension(
-    $container->router,
-    $container->request->getUri()
-  ));
+   function is_cooked()
+   {
+       return $this->cooked;
+   }
 
-    $view->getEnvironment()->addGlobal('auth', [
-    'check' => $container->auth->check(),
-    'user' => $container->auth->user(),
-  ]);
+} // end of class Spinach
 
-    $view->getEnvironment()->addGlobal('flash', $container->flash);
-
-    return $view;
-};
-
-$container['validator'] = function ($container) {
-    return new App\Validation\Validator;
-};
-
-$container['HomeController'] = function ($container) {
-    return new \App\Controllers\HomeController($container);
-};
-
-$container['AuthController'] = function ($container) {
-    return new \App\Controllers\Auth\AuthController($container);
-};
-
-$container['PasswordController'] = function ($container) {
-    return new \App\Controllers\Auth\PasswordController($container);
-};
-
-$container['csrf'] = function ($container) {
-    return new \Slim\Csrf\Guard;
-};
-
-$app->add(new \App\Middleware\ValidationErrorsMiddleware($container));
-$app->add(new \App\Middleware\OldinputMiddleware($container));
-$app->add(new \App\Middleware\CsrfViewMiddleware($container));
-
-$app->add($container->csrf);
-
-v::with('App\\Validation\\Rules\\');
-
-require __DIR__ . '/../app/routes.php';
+?>
